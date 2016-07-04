663685d78f1802cc122db07b6256f3b6bd3467df
Cameron Gale <cameroneldongale@gmail.com>
2016-07-04 16:52:53 -0600

    fixed user model

--- server/models/User.js
+++ server/models/User.js
@@ -3,9 +3,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-  name: String;
-  password: String;
-  email: String;
+  name: String,
+  password: String,
+  email: String,
 })
 
 
